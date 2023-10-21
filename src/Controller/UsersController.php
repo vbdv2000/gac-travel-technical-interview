2ea7732a15710a4c5106753fd1d19e933809cea4
Victor Berenguer <vbdv1@alu.ua.es>
2023-10-21 14:41:30 +0200

    no funciona el registro, no guarda los datos correctamente en el Form, tienes los datos a null

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use App\Form\UsersType;
+use App\Repository\UsersRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/users')]
+class UsersController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(): Response
+    {
+        return $this->render('sign-up.html', [
+            'controller_name' => 'UsersController',
+        ]);
+    }
+
+    #[Route('/new', name: 'app_users_new', methods: ['POST'])]
+    public function new(Request $request, UsersRepository $usersRepository): Response
+    {
+        $user = new Users();
+        $user->setActive(true);
+        $now = new \DateTime();
+        $user->setCreatedAt($now);
+
+
+        $form = $this->createForm(UsersType::class, $user);
+        $form->handleRequest($request); 
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $usersRepository->add($user);
+            return $this->redirectToRoute('success_register', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('sign-up.html', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+
+    #[Route('/{id}', name: 'app_users_show', methods: ['GET'])]
+    public function show(Users $user): Response
+    {
+        return $this->render('users/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_users_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Users $user, UsersRepository $usersRepository): Response
+    {
+        $form = $this->createForm(UsersType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $usersRepository->add($user);
+            return $this->redirectToRoute('app_users_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('users/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_users_delete', methods: ['POST'])]
+    public function delete(Request $request, Users $user, UsersRepository $usersRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $usersRepository->remove($user);
+        }
+
+        return $this->redirectToRoute('app_users_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/UsersType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('active', CheckboxType::class)
+            ->add('created_at', DateTimeType::class)
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
